0e6a25c5a7ea2ab05c215858fa81689808991de3
Carlos Mesquita <carlos.mesquita@vinted.com>
2025-03-14 18:12:52 +0100

    Update Event

--- shared/src/commonMain/kotlin/com/vinted/shops/kmp/omnilog/Event.kt
+++ shared/src/commonMain/kotlin/com/vinted/shops/kmp/omnilog/Event.kt
@@ -1,6 +1,10 @@
 package com.vinted.shops.kmp.omnilog
 
-interface Event {
-    val name: String
-    val properties: Map<String, Any>
+abstract class Event {
+    abstract val name: String
+    abstract val properties: Map<String, Any?>
+
+    override fun toString(): String {
+        return "Event(name=$name, properties=$properties)"
+    }
 }
